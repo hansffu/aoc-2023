345eaee79399c2d9b9fa3aeb1489d0b16dadff77
Hans Fredrik Furholt <hansff@gmail.com>
2023-12-05 19:20:25 +0100

    day 5 part 2 - super duper slow. Went shopping while running

--- src/Solutions/Day5.hs
+++ src/Solutions/Day5.hs
@@ -1,7 +1,10 @@
+{-# LANGUAGE TupleSections #-}
+
 module Solutions.Day5 where
 
 import Data.Ix (inRange)
-import Data.List.Split (splitOn)
+import Data.List.Split (chunksOf, splitOn)
+
 import Lib.Solution (Solution (Solution, day, part1Solution, part2Solution))
 import Lib.TaskRunner (InputType (..), run)
 import Lib.Utils (readInt)
@@ -13,19 +16,36 @@ day5 =
   Solution
     { day = 5
     , part1Solution = part1
-    , part2Solution = return . const 0
+    , part2Solution = part2
     }
 
 test :: IO Int
-test = run part1 $ Sample 5
+test = run part2 $ Sample 5
 
 part1 :: [String] -> IO Int
 part1 input = do
   let seeds = readInt <$> tail (words $ head input)
-  let rangeDefs = fmap (head . toRangeMappings . (readInt <$>) . words) . tail <$> splitOn [""] (drop 2 input)
-  let converters = toDestination <$> rangeDefs
-  let converter = foldl (.) id (reverse converters)
+  let converter = getConverter input
   return $ minimum (converter <$> seeds)
+
+part2 :: [String] -> IO Int
+part2 input = do
+  let seedRanges = (\x -> (head x, head x + x !! 1)) <$> chunksOf 2 (readInt <$> tail (words $ head input)) >>= (\(a, b) -> [a..b])
+  let seeds = readInt <$> tail (words $ head input)
+  -- print seedRanges
+  -- print $ rangeDefs input
+  -- print $ maximum seeds
+  let converter = getConverter input
+  -- return $ minimum (converter <$> (take 100000 seeds))
+  -- let a = foldr1 min (converter <$> seeds)
+  let a = foldl1 (\acc cur -> min acc $ converter cur) seedRanges
+  return a
+
+rangeDefs :: [String] -> [[RangeMapping]]
+rangeDefs input = fmap (head . toRangeMappings . (readInt <$>) . words) . tail <$> splitOn [""] (drop 2 input)
+
+getConverter :: [String] -> (Int -> Int)
+getConverter input = foldl (.) id (reverse (toDestination <$> rangeDefs input))
 
 toRangeMappings :: [Int] -> [RangeMapping]
 toRangeMappings (a : b : c : xs) = (a, b, c) : toRangeMappings xs
