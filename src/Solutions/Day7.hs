8e5ff85ac15fc60d8068cc3cfadfab25effc5366
Hans Fredrik Furholt <hansff@gmail.com>
2023-12-07 23:25:21 +0100

    day 7 part 2

--- src/Solutions/Day7.hs
+++ src/Solutions/Day7.hs
@@ -2,15 +2,12 @@ module Solutions.Day7 where
 
 import Data.Char (digitToInt)
 import Data.List (nub, sort, sortBy)
+import Data.List.Extra (replace)
 import Data.Tuple.Extra (both)
 import Lib.Solution (Solution (..))
-import Lib.TaskRunner (InputType (..), run)
 
 day7 :: Solution Int Int
 day7 = Solution 7 part1 part2
-
-test :: IO Int
-test = run part1 $ Sample 7
 
 part1 :: [String] -> IO Int
 part1 input = do
@@ -18,17 +15,44 @@ part1 input = do
   return $ sum $ zipWith (*) (snd <$> sort rounds) [1 ..]
 
 part2 :: [String] -> IO Int
-part2 input = return 0
+part2 input = do
+  let rounds = transformRound . parseRound <$> input
+  return $ sum $ zipWith (*) (snd <$> sort rounds) [1 ..]
+
+transformRound :: Round -> Round2
+transformRound (Hand cards, bet) = ((\h -> JokerHand h (maximum $ permutations h)) (Hand (jTo0 <$> cards)), bet)
+ where
+  jTo0 = jToX 0
+  jToX x 11 = x
+  jToX _ c = c
+
+permutations :: Hand -> [Hand]
+permutations (Hand cards) =
+  [ Hand $ replace [0] [x] cards
+  | x <- [0 .. 14]
+  , x /= 11
+  ]
 
 newtype Hand = Hand {getCards :: [Int]} deriving (Eq)
+data JokerHand = JokerHand {originals :: Hand, improved :: Hand} deriving (Eq)
+
+compareType :: Hand -> Hand -> Ordering
+compareType handA handB = compare a b
+ where
+  (a, b) = both ((fst <$>) . sortByOccurences . getCards) (handA, handB)
 
 instance Ord Hand where
   compare handA handB = if eqType == EQ then compare (getCards handA) (getCards handB) else eqType
    where
-    (a, b) = both ((fst <$>) . sortByOccurences . getCards) (handA, handB)
-    eqType = compare a b
+    eqType = compareType handA handB
+
+instance Ord JokerHand where
+  compare handA handB = if eqType == EQ then compare (getCards $ originals handA) (getCards $ originals handB) else eqType
+   where
+    eqType = compareType (improved handA) (improved handB)
 
 type Round = (Hand, Int)
+type Round2 = (JokerHand, Int)
 
 parseRound :: String -> Round
 parseRound input = (Hand cards, bet)
